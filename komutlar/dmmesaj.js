2036a7c829ca02408f2d18d1b12cc93ace82c254
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 00:21:46 +0300

    Add files via upload

---
+++ komutlar/dmmesaj.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, args) => {
+if (message.author.id !=ayarlar.sahip) { return; }
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
+  message.delete();
+      const mesajat = new Discord.RichEmbed()
+      .setColor(0xff7f00)
+      .setDescription('' + mesaj + '')
+
+      client.users.forEach(u => {
+u.sendEmbed(mesajat)
+})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['duyur'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'dmduyuru',
+  description: 'İstediğiniz şeyi bota duyurtur.',
+  usage: 'dmduyuru [duyurmak istediğiniz şey]'
+};
