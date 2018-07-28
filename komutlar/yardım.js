99cb230c22fb1e697b8fd1118d80127a7b1881da
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 01:13:46 +0300

    Add files via upload

---
+++ komutlar/yardım.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message) => {
+	const embed = new Discord.RichEmbed()
+	.setTitle('Yardım Menüsü')
+	.addField('Komutlar', `* fs!ping - Botun Pingini Gösterir
+* fs!ban - Kişiyi Banlar
+* fs!kick - Kişiyi Kickler
+* fs!temizle - Sohbeti Temizler
+* fs!yardım - Yardım Menüsünü Gösterir
+* fs!dmduyuru - Bot Herkese Özelden Mesaj Atar. **Bu Komut Sadece <@398949644232687628> ve <@428651826913738763> İçindir`)
+    .setColor("RANDOM")
+	.setFooter('Flammes Yardım Menüsü')
+    message.channel.send(embed)
+})
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'yardım',
+  usage: 'yardım'
+};
