7d159ba6ca161c1af82ccc76fb107acb044009f7
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 22:46:15 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,14 +2,15 @@ const Discord = require('discord.js')
 
 exports.run = (client, message) => {
 	const embed = new Discord.RichEmbed()
-	.setTitle('Yardım Menüsü')
-	.addField('Komutlar', ` 👉 **fs!ping - Botun Pingini Gösterir.**
-👉 **fs!ban - İstediginiz Kişiyi Banlar**.
-👉 **fs!kick - İstediginiz Kişiyi Kickler**
-👉 **fs!temizle - Sohbeti Temizler**
-👉 ** fs!avatarım - Profil Resminizi Gösterir. **
-👉 **fs!yardım - Yardım Menüsünü Gösterir.**
- **Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz.**`)
+	.setTitle('Yardım-Menüsü')
+	.addField('Komutlar', ` 📌 **fs!ping - Botun Pingini Gösterir.** ✅
+📌 **fs!ban - İstediginiz Kişiyi Banlar**. ✅
+📌 **fs!kick - İstediginiz Kişiyi Kickler** ✅
+📌 **fs!temizle - Sohbeti Temizler** ✅
+📌 **fs!avatarım - Profil Resminizi Gösterir.** ✅
+📌 **fs!sunucuicon - Sunucununuzun Resmini Gösterir.** ✅
+📌 **fs!yardım - Yardım Menüsünü Gösterir.** ✅
+ **Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz. ✅**`) 
     .setColor("RANDOM")
 	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
     message.channel.send(embed)
