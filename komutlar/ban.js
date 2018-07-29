a594a75be9fc67bf63411ca04ce19466964bdecf
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 23:19:48 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -7,15 +7,15 @@ exports.run = (client, message, args) => {
   .setColor(0xff7f00)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
+  .addField('**:warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.**')
   return message.author.sendEmbed(ozelmesajuyari); }
   let guild = message.guild
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`Sunucunda mod-log kanalı bulamadım. mod-log Kanalını Ekle');
+  if (!modlog) return message.reply('`**Sunucunda mod-log kanalı bulamadım. mod-log Kanalını Ekle !**');
   if (reason.length < 1) return message.reply('Sebebini Söyle');
-  if (message.mentions.users.size < 1) return message.reply('Kim O Banlayacam Söyle Bana').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('** Kimi Banlayacam Söyle Bana? **').catch(console.error);
 
   if (!message.guild.member(user).bannable) return message.reply(':no_entry_sign: Onlar Yetkililer Onları Banlayamam:no_entry_sign:');
   message.guild.ban(user, 2);
