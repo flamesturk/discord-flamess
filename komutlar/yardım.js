026d2f2e10939dabd0daf3c7edfade59f9d0bd64
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 22:11:08 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js')
 exports.run = (client, message) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('Yardım Menüsü')
-	.addField('Komutlar', ` -  fs!ping - Botun Pingini Gösteri
-- fs!ban - Kişiyi Banlar
-- fs!kick - Kişiyi Kickler
-- fs!temizle - Sohbeti Temizler
-- fs!yardım - Yardım Menüsünü Gösterir
-- fs!dmduyuru - Bot Herkese Özelden Mesaj Atar. **Bu Komut Sadece <@398949644232687628> İçindir**`)
+	.addField('Komutlar', ` kullanım > fs!ping - Botun Pingini Gösteri
+kullanım > fs!ban - Kişiyi Banlar
+kullanım > fs!kick - Kişiyi Kickler
+kullanım > fs!temizle - Sohbeti Temizler
+kullanım > fs!yardım - Yardım Menüsünü Gösterir
+kullanım > fs!dmduyuru - Bot Herkese Özelden Mesaj Atar. **Bu Komut Sadece <@398949644232687628> İçindir**`)
     .setColor("RANDOM")
 	.setFooter('Flamess Yardım Menüsü Discord Adresimiz : https://discord.gg/Ymx5sJP ')
     message.channel.send(embed)
