f83f473eafe10ee92d931f684f55334c8b3454fe
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 23:43:54 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,29 +3,29 @@ const Discord = require('discord.js')
 exports.run = (client, message) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('Yardım-Menüsü')
-	.addField('Komutlar: ', ` ▶️  **fs!ping - Botun Pingini Gösterir.**      ✅
+	.addField('Komutlar: ', ` ▶️  **fs!ping - Botun Pingini Gösterir.**      
 
-▶️  **fs!ban - İstediginiz Kişiyi Banlar**.      ✅
+▶️  **fs!ban - İstediginiz Kişiyi Banlar**.      
 
-▶️  **fs!unban - İstediginiz Kişinin Banını Kaldırır**    ✅
+▶️  **fs!unban - İstediginiz Kişinin Banını Kaldırır.**    
 
-▶️  **fs!kick - İstediginiz Kişiyi Kickler**           ✅
+▶️  **fs!kick - İstediginiz Kişiyi Kickler.**           
 
-▶️  **fs!temizle - Sohbeti Temizler**           ✅
+▶️  **fs!temizle - Sohbeti Temizler.**           
 
-▶️  **fs!avatarım - Profil Resminizi Gösterir.**   ✅
+▶️  **fs!avatarım - Profil Resminizi Gösterir.**   
 
-▶️  **fs!servericon - Serverinizin Resminizi Gösterir.**   ✅
+▶️  **fs!servericon - Serverinizin Resminizi Gösterir.**   
 
-▶️  **fs!sor - Bota Soru Sora Bilirsiniz.** ✅
+▶️  **fs!sor - Bota Soru Sora Bilirsiniz.** 
 
-▶️  **fs!yaz - İstediğiniz şeyi bota yazdırır.** ✅
+▶️  **fs!yaz - İstediğiniz şeyi bota yazdırır.** 
 
-▶️  **fs!hesapla - Hesap Makinesi Belirtilen İşlemi yapar.** ✅
+▶️  **fs!hesapla - Hesap Makinesi Belirtilen İşlemi yapar.** 
 
-▶️  **fs!yardım - Yardım Menüsünü Gösterir.** ✅
+▶️  **fs!yardım - Yardım Menüsünü Gösterir.** 
 
-▶️  **Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz. ✅**`) 
+▶️  **Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz. **`) 
     .setColor("RANDOM")
 	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
     message.channel.send(embed)
