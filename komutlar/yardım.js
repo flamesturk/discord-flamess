30ead0e3f94fba1ea30fef66faa51ad41826ee05
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 22:53:56 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,21 +3,21 @@ const Discord = require('discord.js')
 exports.run = (client, message) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('Yardım-Menüsü')
-	.addField('Komutlar', ` ▶️ **fs!ping - Botun Pingini Gösterir.** ✅
+	.addField('Komutlar', ` ▶️  **fs!ping - Botun Pingini Gösterir.** ✅
 
-▶️ **fs!ban - İstediginiz Kişiyi Banlar**. ✅
+▶️  **fs!ban - İstediginiz Kişiyi Banlar**. ✅
 
-▶️ **fs!kick - İstediginiz Kişiyi Kickler** ✅
+▶️  **fs!kick - İstediginiz Kişiyi Kickler** ✅
 
-▶️ **fs!temizle - Sohbeti Temizler** ✅
+▶️  **fs!temizle - Sohbeti Temizler** ✅
 
-▶️**fs!avatarım - Profil Resminizi Gösterir.** ✅
+▶️  **fs!avatarım - Profil Resminizi Gösterir.** ✅
 
-▶️**fs!sunucuicon - Sunucununuzun Resmini Gösterir.** ✅
+▶️  **fs!test - test.** ✅
 
-▶️**fs!yardım - Yardım Menüsünü Gösterir.** ✅
+▶️  **fs!yardım - Yardım Menüsünü Gösterir.** ✅
 
-▶️**Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz. ✅**`) 
+▶️  **Yeni Komutlar Gelicektir,İstek Komutlarınızı <@398949644232687628>e Bildiriniz. ✅**`) 
     .setColor("RANDOM")
 	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
     message.channel.send(embed)
