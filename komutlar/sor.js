c6b6ff3fd1f04c1d1deb4bd5eef2f8fb72d41a7e
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 23:37:04 +0300

    Create sor.js

---
+++ komutlar/sor.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+
+const cevaplar = [
+    "evet",
+    "hmm .. hayır",
+    "gibi gibi",
+    "anlamadım knk?",
+    "bu soru mu ?",
+    "belki",
+    "olabilir",
+    "tabi tabi :D ",
+    "imkansız"
+];
+
+exports.run = function(client, message, args) {
+    var soru = args.join(' ');
+
+    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
+
+    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanım**: fs!soru <soru>')
+    else message.channel.send(cevap)
+
+};  
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: true, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'sor', 
+  description: 'Sihirli 8ball sorularınızı cevaplar',
+  usage: 'sor <soru>'
+};
