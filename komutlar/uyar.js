7d6d6a5def081d982ef45827316c2131f0a589bc
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-30 02:12:17 +0300

    Update uyar.js

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -5,15 +5,15 @@ exports.run = (client, message, args) => {
   .setColor(0xFF0000)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`uyar` adlı komutu özel mesajlarda kullanamazsın.')
+  .addField('** :warning: Uyarı :warning:', '`uyar` adlı komutu özel mesajlarda kullanamazsın.** ')
   return message.author.sendEmbed(ozelmesajuyari); }
   let guild = message.guild
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log kanalını bulamıyom Serverda. mod-log kanalını Ekle');
-  if (reason.length < 1) return message.reply('Uyaradam Kişiyi Söyle Ahlaksız :D');
-  if (message.mentions.users.size < 1) return message.reply('Kime Ceza Verdem').catch(console.error);
+  if (!modlog) return message.reply('** `mod-log kanalını bulamıyom Serverda. mod-log kanalını Ekle**');
+  if (reason.length < 1) return message.reply('**Kimi Uyarıcam La ?**');
+  if (message.mentions.users.size < 1) return message.reply('**Kime Ceza Verim?**').catch(console.error);
   const embed = new Discord.RichEmbed()
   .setColor(0xff7f00)
   .setTimestamp()
