825f3a0d2b21c3a5cccd71c36475d1c8c146db0d
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-29 23:31:56 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,19 +3,19 @@ const Discord = require('discord.js')
 exports.run = (client, message) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('Yardım-Menüsü')
-	.addField('Komutlar', ` ▶️  **fs!ping - Botun Pingini Gösterir.** ✅
+	.addField('Komutlar: ', ` ▶️  **fs!ping - Botun Pingini Gösterir.**      ✅
 
-▶️  **fs!ban - İstediginiz Kişiyi Banlar**. ✅
+▶️  **fs!ban - İstediginiz Kişiyi Banlar**.      ✅
 
-▶️  **fs!unban - İstediginiz Kişinin Banını Kaldırır** ✅
+▶️  **fs!unban - İstediginiz Kişinin Banını Kaldırır**    ✅
 
-▶️  **fs!kick - İstediginiz Kişiyi Kickler** ✅
+▶️  **fs!kick - İstediginiz Kişiyi Kickler**           ✅
 
-▶️  **fs!temizle - Sohbeti Temizler**   ✅
+▶️  **fs!temizle - Sohbeti Temizler**           ✅
 
-▶️  **fs!avatarım - Profil Resminizi Gösterir.** ✅
+▶️  **fs!avatarım - Profil Resminizi Gösterir.**   ✅
 
-▶️  **fs!servericon - Serverinizin Resminizi Gösterir.** ✅
+▶️  **fs!servericon - Serverinizin Resminizi Gösterir.**   ✅
 
 ▶️  **fs!sor - Bota Soru Sora Bilirsiniz.** ✅
 
