b6f9e03b3e511e87fa73d361f1b4d568783428b0
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-30 20:13:06 +0300

    Update sigaraiç.js

--- komutlar/sigaraiç.js
+++ komutlar/sigaraiç.js
@@ -1,37 +1,14 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message, args) => {
-	if (!message.guild) {
-    const ozelmesajuyari = new Discord.RichEmbed()
-    .setColor(0xFF0000)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .addField('**Eğlence Komutları Özel Mesajlarda Kullanılamaz!**')
-    return message.author.sendEmbed(ozelmesajuyari); }
-    if (message.channel.type !== 'dm') {
-      const sunucubilgi = new Discord.RichEmbed()
-    .setAuthor(message.author.username + ` Yak Aga Yak Bişey Olmaz!`)
-    .setColor(3447003)
-    .setDescription('')
-  .setImage(`https://images-ext-2.discordapp.net/external/0KlG_YOVx3HS5hxVpITZ4hYmtj-84d__YdVgfqzcSiQ/https/media.giphy.com/media/Josf4Dji2FyHS/giphy.gif?width=400&height=225`)
-    return message.channel.sendEmbed(sunucubilgi);
-    }
-
-    let username = member.user.username;
-    member.send('Sunucuya Hoş Geldin!');
-    member.guild.defaultChannel.send('');
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['sigaraiç', 'smile'],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'kahkaha',
-  description: 'Kahkaha Atarsınız.',
-  usage: 'kahkaha'
-};
+client.on('message', msg => {
+if (msg.content.toLowerCase() === prefix + "sigara") {
+msg.channel.send(':smoking: :cloud::cloud::cloud:')
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
+.then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
+}
+});
