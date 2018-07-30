44e1cd3c6776702710e113ff96cf9791e53bc3ed
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-30 16:58:45 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,6 +1,26 @@
 const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message) => {
+exports.run = (client, message, params) => {
+ if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+};
 	const embed = new Discord.RichEmbed()
 	.setTitle('Flamess - Yardım-Menüsü')
 	.addField(' Komutlar: ', ` **fs!ping** - Botun Pingini Gösterir.      
