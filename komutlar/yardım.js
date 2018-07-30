09db205c9411f4decfee96eef5191f6ce506770e
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-30 18:26:41 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,24 +6,24 @@ var prefix = ayarlar.prefix;
 exports.run = (client, message,) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('● 𝐅𝐥𝐚𝐦𝐞𝐬𝐬-𝐘𝐚𝐫𝐝ı𝐦-𝐌𝐞𝐧𝐮̈𝐬𝐮̈ ● ')
-	.addField('● 𝐘𝐞𝐭𝐤𝐢𝐥𝐢-𝐊𝐨𝐦𝐮𝐭𝐥𝐚𝐫ı ●: ', ` **●fs!ping** - Botun Pingini Gösterir.      
- **●fs!ban** - İstediginiz Kişiyi Banlar.     
-**●fs!unban** - İstediginiz Kişinin Banını Kaldırır.    
-**●fs!kick** - İstediginiz Kişiyi Kickler.  
-**●fs!uyar** - İstediginiz Kişiyi Uyarır.          
-**●fs!temizle** - Sohbeti Temizler.          
-**●fs!yardım** - fs!y Yardım Menüsünü Gösterir. 
-**●fs!botdavet** - Botu Size Davet Linkini Atar.
-**●fs!botsunucu** - Bot Sunucunun Davet Linkini Atar.
-**●Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**`)
+	.addField('● 𝐘𝐞𝐭𝐤𝐢𝐥𝐢-𝐊𝐨𝐦𝐮𝐭𝐥𝐚𝐫ı ●: ', ` ● **fs!ping** - Botun Pingini Gösterir.      
+ ● **fs!ban** - İstediginiz Kişiyi Banlar.     
+● **fs!unban** - İstediginiz Kişinin Banını Kaldırır.    
+● **fs!kick** - İstediginiz Kişiyi Kickler.  
+● **fs!uyar** - İstediginiz Kişiyi Uyarır.          
+● **fs!temizle** - Sohbeti Temizler.          
+● **fs!yardım** - fs!y Yardım Menüsünü Gösterir. 
+● **fs!botdavet** - Botu Size Davet Linkini Atar.
+● **fs!botsunucu** - Bot Sunucunun Davet Linkini Atar.
+● **Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**`)
 .addField(' ● 𝐄𝐠𝐥𝐞𝐧𝐜𝐞-𝐊𝐨𝐦𝐮𝐭𝐥𝐚𝐫ı ● : ', ` **fs!stresçarkı** - Sizin için bir stres çarkı çevirir.
-**●fs!emojiyazı** - Mesajınızı emoji haline getir.
-**●fs!nsfw** - +18 NSFW - Sapik Resimleri Gösteriri.
-**●fs!hesapla** - Hesap Makinesi Belirtilen İşlemi yapar.
-**●fs!yaz** - İstediğiniz şeyi bota yazdırır.
-**●fs!sor** - Bota Soru Sora Bilirsiniz. 
-**●fs!avatarım** - Profil Resminizi Gösterir.   
-**●fs!servericon** - Serverinizin Resminizi Gösterir.
+● **fs!emojiyazı** - Mesajınızı emoji haline getir.
+● **fs!nsfw** - +18 NSFW - Sapik Resimleri Gösteriri.
+● **fs!hesapla** - Hesap Makinesi Belirtilen İşlemi yapar.
+● **fs!yaz** - İstediğiniz şeyi bota yazdırır.
+● **fs!sor** - Bota Soru Sora Bilirsiniz. 
+● **fs!avatarım** - Profil Resminizi Gösterir.   
+● **fs!servericon** - Serverinizin Resminizi Gösterir.
  `)
     .setColor("BLACK")
 	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
