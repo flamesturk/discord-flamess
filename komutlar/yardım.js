56c2526712034c98961ce97b1bf7829c897132a2
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-30 17:16:57 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -27,28 +27,7 @@ exports.run = (client, message,) => {
     .setColor("BLACK")
 	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
     message.channel.send(embed)
-	.setTitle('Flamess - Yardım-Menüsü2')
-	.addField(' Komutlar2: ', ` **fs!ping** - Botun Pingini Gösterir.      
-**fs!ban** - İstediginiz Kişiyi Banlar.     
-**fs!unban** - İstediginiz Kişinin Banını Kaldırır.    
-**fs!kick** - İstediginiz Kişiyi Kickler.  
-**fs!uyar** - İstediginiz Kişiyi Uyarır.          
-**fs!temizle** - Sohbeti Temizler.       
-**fs!avatarım** - Profil Resminizi Gösterir.   
-**fs!servericon** - Serverinizin Resminizi Gösterir.  
-**fs!sor** - Bota Soru Sora Bilirsiniz. 
-**fs!yaz** - İstediğiniz şeyi bota yazdırır.
-**fs!emojiyazı** - Mesajınızı emoji haline getir.
-**fs!stresçarkı** - Sizin için bir stres çarkı çevirir. 
-**fs!hesapla** - Hesap Makinesi Belirtilen İşlemi yapar. 
-**fs!yardım** - fs!y Yardım Menüsünü Gösterir. 
-**fs!nsfw** - +18 NSFW - Sapik Resimleri Gösteriri.
-**fs!botdavet** - Botu Size Davet Linkini Atar.
-**fs!botsunucu** - Bot Sunucunun Davet Linkini Atar.
-**Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**`)
-    .setColor("BLACK")
-	.setFooter('Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP ')
-    message.channel.send(embed)
+	
 }
 
 exports.conf = {
