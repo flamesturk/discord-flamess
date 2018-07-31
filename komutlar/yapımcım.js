76e131e428dcf00db7bbcfd5262a958111cf466c
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 22:25:21 +0300

    Update yapımcım.js

--- komutlar/yapımcım.js
+++ komutlar/yapımcım.js
@@ -7,9 +7,8 @@ exports.run = (client, message, params) => {
   const embed = new Discord.RichEmbed()
   .setDescription('')
   .setColor(0x00ffff)
-  .addField("**》 Yapımcım 《**", `<@398949644232687628>`)
-  .addField(' Banka : KUVEYTTÜRK Açıklama :Bota Vds İcin. 
-    IBAN: TR790020500009513048100001 `)
+  .addField("**》 Yapımcım 《**", `<@398949644232687628> **Banka : KUVEYTTÜRK Açıklama: Bota Vds İcin. 
+    IBAN: TR790020500009513048100001** `)
  
   return message.channel.sendEmbed(embed);
 };
