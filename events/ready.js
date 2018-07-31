5e2da5414dc281dc5a25e96f29447f90e113fa71
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 17:59:42 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -17,14 +17,11 @@ console.log('Bot hazır ve giriş yaptı.');
 console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 
     var Games = [
-
         "fs!yardım 》fs!davet《 fs!botsunucu.",
         "Discord Sunucumuz 》 https://discord.gg/Ymx5sJP 《",
         "》 Beni Davet Et fs!davet 《",
         
         `${prefix}yardım | ${client.guilds.size} Sunucuda | fs!davet `
-
-
     ];
 
     setInterval(function() {
