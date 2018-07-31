e7da8d2afa9a5d0478b1cc4674243fb141b883bb
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 09:10:47 +0300

    Update ready.js

--- olaylar/ready.js
+++ olaylar/ready.js
@@ -6,32 +6,10 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-    snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-  .set('Authorization', api)
-  .send({ server_count: client.guilds.size })
-  .then(() => console.log('Updated discordbots.org stats.'))
-console.log('>>Oynuyor kısmı başarıyla güncellendi.');
-console.log('Bot hazır ve giriş yaptı.');
-console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-
-    var Games = [
-
-        "fs!yardım 》fs!botsunucu《 Botun Sunucusu! .",
-        "Bot Davet Linki 》 fs!davet 《",
-        "》 Yep Yeni Özellikler Yakında!《",
-        "》 https://discord.gg/Ymx5sJP 《",
-
-
-        `${prefix}yardım | ${client.guilds.size} sunucu | Yeni Komutlar Yakında`
-
-
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
-
-        client.user.setGame(Games[random], "https://www.twitch.tv/hotbrosflamess");
-        }, 2 * 2500);
-
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`${prefix}yardım | ${client.guilds.size} sunucu`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
