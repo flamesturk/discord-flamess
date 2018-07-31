bafdc20385b92c8dac0a36355074a4fb49bcf8ad
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 17:16:01 +0300

    Update and rename olaylar/ready.js to  olaylar/ready.js

--- olaylar/ready.js
+++ olaylar/ready.js
@@ -2,14 +2,38 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+const snekfetch = require('snekfetch');
+const api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDIzMTYzMDQwNTA0MjE3NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE2Mjc0MTQ1fQ.2H9LjNjH6WFp5LmswfXAYSDsHQn2JSPPgbgf1WjSi_c';
 
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setGame(`${prefix}yardım | ${client.guilds.size} sunucu`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+    snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+  .set('Authorization', api)
+  .send({ server_count: client.guilds.size })
+  .then(() => console.log('Updated discordbots.org stats.'))
+console.log('>>Oynuyor kısmı başarıyla güncellendi.');
+console.log('Bot hazır ve giriş yaptı.');
+console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+
+    var Games = [
+
+        "vgs!yardım 》 Eklenecektir 《 Resmi Web Sitemiz.",
+        "Bot Davet Linki 》 Ekelenecektir 《",
+        "》 Yep Yeni Özellikler 《",
+        "》 Vegas Bot Yapım Olarak Sizlere Hizmet Vermekteyiz. 《",
+
+
+        `${prefix}yardım | ${client.guilds.size} sunucu | Yeni Komutlar Yakında`
+
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+
+        client.user.setGame(Games[random], "https://www.twitch.tv/VegasBot");
+        }, 2 * 2500);
+
+}
