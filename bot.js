4ce861876d7c6e5e37df33f8434f68edb4a49736
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 01:35:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,18 @@ fs.readdir('./komutlar/', (err, files) => {
   });
 });
 
+client.on('message', msg => {
+if (msg.content.toLowerCase() === prefix + "sigara") {
+msg.channel.send(':smoking: :cloud::cloud::cloud:')
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
+.then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
+}
+});
+
     client.on("message", message => {
     const dmchannel = client.channels.find("name", "botlog");
     if (message.channel.type === "dm") {
