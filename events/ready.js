2e00f42ef65ebb4e952d8d7d554a417adb7b442f
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 18:12:13 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -18,10 +18,10 @@ console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.
 
     var Games = [
         "fs!yardım 》fs!davet《 fs!botsunucu",
-        "Discord Sunucumuz 》 https://discord.gg/Ymx5sJP 《",
-        "》 Beni Davet Et fs!davet 《",
+        "Discord Sunucumuz》https://discord.gg/Ymx5sJP《",
+        "》 Beni Discorduna Davet Et fs!davet 《",
         
-        `》${prefix}yardım 《|》${client.guilds.size} Sunucuda《|》fs!davet《 `
+        `》${prefix}yardım《》${client.guilds.size} Sunucuda《》fs!davet《`
     ];
 
     setInterval(function() {
