6a7e74a33fb57f1bf121af58d1a60c2b1ae30abb
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 00:00:00 +0300

    Update ses-kanal-aç.js

--- komutlar/ses-kanal-aç.js
+++ komutlar/ses-kanal-aç.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
     let kanal = args.slice(0).join(' ');
     let guild = message.guild;
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply('Bunun için gerekli iznin yok');
-	if (kanal.length < 1) return message.reply('Lütfen oluşturacağım kanalın adını yaz.');
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply('**Bunun için gerekli iznin yok**');
+	if (kanal.length < 1) return message.reply('**Lütfen oluşturacağım kanalın adını yaz.?**');
   message.delete();
   guild.createChannel(kanal, 'voice');
   message.channel.send("Kanal oluşturuldu");
