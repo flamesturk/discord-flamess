e7762b9bab0e969ee3bb06724cc17592a04902d8
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-07-31 17:58:11 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -18,12 +18,10 @@ console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.
 
     var Games = [
 
-        "fs!yardım 》 《 fs!botsunucu.",
-        "Bot Davet Linki 》 Ekelenecektir 《",
-        "》 Yep Yeni Özellikler 《",
-        "》 Flamess Bot Yapım Olarak Sizlere Hizmet Vermekteyiz. 《",
-
-
+        "fs!yardım 》fs!davet《 fs!botsunucu.",
+        "Discord Sunucumuz 》 https://discord.gg/Ymx5sJP 《",
+        "》 Beni Davet Et fs!davet 《",
+        
         `${prefix}yardım | ${client.guilds.size} Sunucuda | fs!davet `
 
 
