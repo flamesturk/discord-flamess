3bffd1d94e2163876e26321604f0d2f8f204bd0f
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 02:49:24 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,4 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("idle");
 }
