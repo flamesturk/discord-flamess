dbbdacc4407eb950a5aeb0de0e3f0afff385afad
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 00:39:05 +0300

    Create espiri.js

---
+++ komutlar/espiri.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+exports.run = function(client, message,) {
+
+client.on('message', message => {
+if (message.content.toLowerCase() === prefix + "espriyap") {
+    var sans = ["Geçen gün geçmiş günlerimi aradım ama meşguldü.", "Yağmur yağmış kar peynir", "Dünya dönermiş ay da köfte…", "Bu erikson başka erik yok.", "Yıkanan Ton a ne denir Washington", "Hadi oyun oynayalım. Vazgeçtim oymadan oynayalım!", "Geçen gün kamyonu sürdüm Leonardo da Vinci.", "Doğumdan sonra çok kilo aldım. Doğduğumda 2 kiloydum şimdi 62.", "Adam 7 gün boyunca nezle olmuş. Sıkılmış bugün de Petek le olayım demiş.", "Yarasa yararlı bir hayvandır. Yararlı bir hayvan olmasaydı yaramasa derlerdi.", " Benim neden kardeşim yok baba  Seni görünce ikincisine cesaret edemedik.", "Osmanlıda kimseye borç takamıyordun mesela sikke sikke ödüyodun…", "Tatlı yiyip, tatlı konuşuluyorsa bundan sonra mantı yiyip mantıklı konuşacağız.", "Babamı sahura kaldırmayı unuttuk anneme masada ne eksik diyorum tuzluk mu diyor.", "+Okeyde kıza elin nasıl dedim. Ojeli dedi. Ben Şoka girdim. O Migrosa.", "Canım sıkkın kanka sonra gel"];
+    var sonuc = sans[Math.floor((Math.random() * sans.length))];
+    const embed = new Discord.RichEmbed()
+    .addField(`***___Espri___***`, `${sonuc}`)
+    return message.channel.sendEmbed(embed);
+}
+});
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [ ' espiriyap ' ],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'espiri',
+  description: 'Espiri Yapar.',
+  usage: 'espiriyap'
+};
