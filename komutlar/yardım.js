eb12d48f19c71fd23ae4a5e10a61e1e7d4491bc3
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 23:30:12 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -72,7 +72,6 @@ Serverinizin Resminizi Gösterir.`)
 [minecraft adınız] Minecarft Skininizi Gösterir.`)
 .addField('》fs!mcavatar《 ', `
 [minecraft adınız] Minecraft Avatarınızı Gösterir.`)
-**Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**
 .setColor("RANDOM")
 .setFooter(' -Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP-')
     message.channel.send(embed)
