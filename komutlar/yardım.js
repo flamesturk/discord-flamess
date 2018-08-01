be7960043530f3184b876d40b78fcc8685e5a9cb
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 03:05:19 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,44 +7,43 @@ exports.run = (client, message,) => {
 	const embed = new Discord.RichEmbed()
 	.setTitle('● 𝐅𝐥𝐚𝐦𝐞𝐬𝐬-𝐘𝐚𝐫𝐝ı𝐦-𝐌𝐞𝐧𝐮̈𝐬𝐮̈ ● ')
 	.addField('● 𝐘𝐞𝐭𝐤𝐢𝐥𝐢-𝐊𝐨𝐦𝐮𝐭𝐥𝐚𝐫ı ●: ', ` 》● **fs!ping** - Botun Pingini Gösterir.  
- 》● **fs!ban** - İstediginiz Kişiyi Banlar.     
-》● **fs!unban** - İstediginiz Kişinin Banını Kaldırır.    
-》● **fs!kick** - İstediginiz Kişiyi Kickler.  
-》● **fs!uyar** - İstediğiginiz Kişiyi Uyarırsınız.
-》● **fs!duyuru** - Güzel Bir Duyuru Görünümü Sağlar.
-》● **fs!istatislik** - Botun Bilgisini Verir.
-》● **fs!ses-kanal-aç** - Bir ses kanalı açar.
-》● **fs!yazı-kanal-aç** - Bir yazı kanalı açar.
-》● **fs!rol-ver** - İstediğiniz kişiyi istediğiniz rolü verir.         
-》● **fs!temizle** - Sohbeti Temizler.          
-》● **fs!yardım** - fs!y Yardım Menüsünü Gösterir. 
-》● **fs!Özeldenyardım** Yakında Gelicek.
-》● **fs!botsunucu** - Botun Sunucusunun Davet Linkini Atar Gelmeyi Unutmayın.
-》● **fs!davet** - Botun Davet Linkini Atar.
-》● **fs!tavsiye** - Bot hakkında tavsiye verebilirsiniz. size en yakın zaman icinde cevap vericegim. 
-》● **fs!yapımcım** - Botun Yapımcısını Gösterir.`) 	
+ 1》● **fs!ban** - İstediginiz Kişiyi Banlar.     
+2》● **fs!unban** - İstediginiz Kişinin Banını Kaldırır.    
+3》● **fs!kick** - İstediginiz Kişiyi Kickler.  
+4》● **fs!uyar** - İstediğiginiz Kişiyi Uyarırsınız.
+5》● **fs!duyuru** - Güzel Bir Duyuru Görünümü Sağlar.
+6》● **fs!istatislik** - Botun Bilgisini Verir.
+7》● **fs!ses-kanal-aç** - Bir ses kanalı açar.
+8》● **fs!yazı-kanal-aç** - Bir yazı kanalı açar.
+9》● **fs!rol-ver** - İstediğiniz kişiyi istediğiniz rolü verir.         
+10》● **fs!temizle** - Sohbeti Temizler.          
+11》● **fs!yardım** - fs!y Yardım Menüsünü Gösterir. 
+12》● **fs!Özeldenyardım** Yakında Gelicek.
+13》● **fs!botsunucu** - Botun Sunucusunun Davet Linkini Atar Gelmeyi Unutmayın.
+14》● **fs!davet** - Botun Davet Linkini Atar.
+15》● **fs!tavsiye** - Bot hakkında tavsiye verebilirsiniz. size en yakın zaman icinde cevap vericegim. 
+16》● **fs!yapımcım** - Botun Yapımcısını Gösterir.`) 	
 .addField(' ● 𝐄𝐠𝐥𝐞𝐧𝐜𝐞-𝐊𝐨𝐦𝐮𝐭𝐥𝐚𝐫ı ● : ', ` 》●**fs!stresçarkı** - Sizin için bir stres çarkı çevirir.
-》● **fs!emojiyazı** - Mesajınızı emoji haline getirir.
-》● **fs!sigaraiç veya fs!sigara** - Bot Size Sigara İçirir.
-》● **fs!yumruk-at** - Yumruk Atar gif. 
-》● **fs!koş** - Koşarsınız gif.
-》● **fs!herkesebendençay** - Herkese Çay Verir gif.
-》● **fs!söv** - İstediginiz Kişiye Söverim Bence Yapma.
-》● **fs!nahçek** - İstediğiniz Kişiye Nah Çekersiniz.
-》● **fs!kullanıcıbilgim** - kbilgim Komutu kullanan kişi hakkında bilgi.
-》● **fs!sunucubilgi** Sunucu hakkında bilgi verir.
-》● **fs!nsfw** - +18 NSFW - Sapik Resimleri Gösteririr.
-》● **fs!hesapla** - Hesap Makinesi Belirtilen İşlemi yapar.
-》● **fs!yaz** - İstediğiniz şeyi bota yazdırır. 
-》● **fs!sor** - Bota Soru Sora Bilirsiniz. 
-》● **fs!mcskin** - Minecrafttaki Skininizi Gösterir. 
-》● **fs!mcavatar** - Minecraft Kafanızı Gösterir.
-》● **fs!avatarım** - Profil Resminizi Gösterir.   
-》● **fs!servericon** - Serverinizin Resminizi Gösterir.
-**Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**
- `)
-    .setColor("BLACK")
-	.setFooter(' -Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP-')
+17》● **fs!emojiyazı** - Mesajınızı emoji haline getirir.
+18》● **fs!sigaraiç veya fs!sigara** - Bot Size Sigara İçirir.
+19》● **fs!yumruk-at** - Yumruk Atar gif. 
+20》● **fs!koş** - Koşarsınız gif.
+21》● **fs!herkesebendençay** - Herkese Çay Verir gif.
+22》● **fs!söv** - İstediginiz Kişiye Söverim Bence Yapma.
+23》● **fs!nahçek** - İstediğiniz Kişiye Nah Çekersiniz.
+24》● **fs!kullanıcıbilgim** - kbilgim Komutu kullanan kişi hakkında bilgi.
+25》● **fs!sunucubilgi** Sunucu hakkında bilgi verir.
+26》● **fs!nsfw** - +18 NSFW - Sapik Resimleri Gösteririr.
+27》● **fs!hesapla** - Hesap Makinesi Belirtilen İşlemi yapar.
+28》● **fs!yaz** - İstediğiniz şeyi bota yazdırır. 
+29》● **fs!sor** - Bota Soru Sora Bilirsiniz.
+32》● **fs!avatarım** - Profil Resminizi Gösterir. 
+33》● **fs!servericon** - Serverinizin Resminizi Gösterir.
+**Flamess Botun Sahibi Owner: <@398949644232687628> | Developer: <@428651826913738763>**`)
+.addField('Minecraft Komutları', `fs!mcskin - Minecarft Skininizi Gösterir
+fs!mcavatar - Minecraft Avatarınızı Gösterir`)
+.setColor("BLACK")
+.setFooter(' -Flamess Discord Yardım Menüsü - Discord Adresimiz : https://discord.gg/Ymx5sJP-')
     message.channel.send(embed)
 	
 }
