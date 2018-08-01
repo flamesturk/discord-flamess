aa7a153b97842245faee35c1a87e0b012b17b11d
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 22:27:17 +0300

    Update and rename duyuru.js to kutuiciyaz.js

--- komutlar/kutuiciyaz.js
+++ komutlar/kutuiciyaz.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
   let mesaj = args.slice(0).join(' ');
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply('Bunun için gerekli iznin yok');
-	if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın!');
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply('**Bunun için gerekli iznin yok**');
+	if (mesaj.length < 1) return message.reply('**Yazmam İçin Birşey Yazmalısın!**');
     message.delete();
     const embed = new Discord.RichEmbed()
     .setAuthor('')
@@ -15,12 +15,12 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['duyuruyap', 'duyur'],
+  aliases: ['kutuiciyaz', 'kutuici'],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'duyuru',
-  description: 'Güzel Bir Duyuru Görünümü Sağlar.',
-  usage: 'duyuru [Duyuruda Yazıcak Şey]'
+  name: 'kutu',
+  description: 'Kutu icinde yazarsınız.',
+  usage: 'kutuiciyaz [Kutu icinde Yazılacak Şey]'
 };
