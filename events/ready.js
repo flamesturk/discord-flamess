69115004d20edca7a58166002b4e2ed7b9a34b4a
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-01 13:40:25 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,20 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+const snekfetch = require('snekfetch');
+const api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDIzMTYzMDQwNTA0MjE3NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE2Mjc0MTQ1fQ.2H9LjNjH6WFp5LmswfXAYSDsHQn2JSPPgbgf1WjSi_c';
 
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-}
+    snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+  .set('Authorization', api)
+  .send({ server_count: client.guilds.size })
+  .then(() => console.log('Updated discordbots.org stats.'))
+console.log('>>Oynuyor kısmı başarıyla güncellendi.');
+console.log('Bot hazır ve giriş yaptı.');
+console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+
     var Games = [
 
         "fs!yardım 》 fs!davet 《 Davet etmek için",
