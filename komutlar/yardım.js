e09d339e480993e8ac4aaba34b13d2c3c5c12aa7
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-03 00:23:21 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -12,7 +12,7 @@ exports.run = (client, message, args, tools) => {
 
   const embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .setFooter(`Sayfa ${page} / ${pages.length}, "")
+  .setFooter('Sayfa ${page} / ${pages.length}, "")
   .setDescription(pages[page-1])
   .setAuthor("Rick And Morty <3", "")
 message.channel.send(embed).then(msg => {
