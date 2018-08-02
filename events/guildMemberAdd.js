005cddfead70858730cc5b5bbe9dcb371cfb4f6a
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-02 10:42:54 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,5 +4,5 @@ var prefix = ayarlar.prefix;
 
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('Hey Dostum **' + username + '** Sunucuya Hoşgeldin benim sunucumada gelebilirsin https://discord.gg/Ymx5sJP beni eklemek icin fs!davet **');
+    member.sendMessage('Hey Dostum **' + username + '** Sunucuya Hoşgeldin beni eklemek icin fs!davet **');
 };
