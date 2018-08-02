5ecefd44655476a6a8c43526ee5649785a5c7a8d
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-02 13:18:14 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,14 +7,9 @@ exports.run = (client, message, params) => {
   const embedyardim = new Discord.RichEmbed()
   .setTitle("Komutlar")
   .setDescription('')
-  .setColor('RANDOM')
-  .addField("**» Eğlence Komutları**", `fs!starwars = StarWars (Pixel Formatında) Filmini Gösterir. \fs!herkesebendençay = Herkese Çay Alırsınız. \fs!koş = Koşarsınız.\fs!yumruk-at = Yumruk Atarsınız. \fs!söv = Bot Etiketlediğiniz Kişiye Söver.`)
-
-  .addField("**» Kullanıcı Komutları**", `fs!report = İstediğiniz Kullanıcıyı Reportlarsınız. \fs!yaz = Bota İsediğinizi Yazdırırsınız. \fs!sunucubilgi = Bulunduğunuz Sunucu Hakkında Bilgi Verir. \fs!sunucuresmi = Bulunduğunuz Sunucunun Resmin Gösterir. \fs!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. \fs!avatarım = Avatarınınızı Gösterir. `)
-  .addField("**» Sunucu Yetkilisi Komutları**", `fs!ban = İstediğiniz Kişiyi Sunucudan Banlar. \fs!kick  = İstediğiniz Kişiyi Sunucudan Atar. \fs!unban = İstediğiniz Kişinin Yasağını Açar. \fs!sustur = İstediğiniz Kişiyi Susturur. \fs!sil = Belirtilen Miktarda Mesajı Silir. (MAX 100) \fs!oylama = Oylama Açar. \fs!kutuiciyaz = Kutu içinde yazı yazarsınız. \fs!rol-ver = İstediğiniz Kişiye Rol Verebilirsiniz`)
-  .addField("**» Botun Ana Komutları**", "fs!yardım = BOT Komutlarını Atar. \fs!bilgi = BOT Kendisi Hakkında Bilgi Verir. \fs!ping = BOT Gecikme Süresini Söyler. \fs!davet = BOT Davet Linkini Atar. \fs!istatistik = BOT İstatistiklerini Gösterir. \fs!yapımcım = Botun Yapımcısını Gösterir")
-  .addField("**» Oyun Komutları**", `fs!fortnite = İstediğiniz Kullanıcının İstatistiklerine Bakarsınız.`)
-  .setFooter('Flamess Bot Güncel Sürüm [ BETA v0.3.1]')
+  .setColor(0x00ffff)
+  .addField("**» Eğlence Komutları**", `vgs!atatürk = Rastgele Atatürkün Fotoğraflarını Gösterir. \nvgs!starwars = StarWars (Pixel Formatında) Filmini Gösterir. \nvgs!banned = Dene ve Görvgs! \nvgs!kahkaha = Kahkaha Atarsınız \nvgs!herkesebendençay = Herkese Çay Alırsınız. \nvgs!koş = Koşarsınız.\nvgs!çayiç = Çay İçersiniz. \nvgs!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nvgs!çayaşekerat = Çaya Şeker Atarsınız. \nvgs!yumruk-at = Yumruk Atarsınız. \nvgs!şanslısayım = Bot Sizin Şanslı Sayınızı Söyler. \nvgs!söv = Bot Etiketlediğiniz Kişiye Söver.`)
+  
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
