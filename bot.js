fa66d8adebe93a0b7b2578fa285e778af1f14094
flamesturk <41647518+flamesturk@users.noreply.github.com>
2018-08-03 22:04:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,23 +27,6 @@ fs.readdir('./komutlar/', (err, files) => {
   });
 });
 
-client.on('message', async message => {
-    if (message.content.toLowerCase() === prefix + 'döviz') {
-var request = require('request');
-request('https://www.doviz.com/api/v1/currencies/USD/latest', function (error, response, body) {
-    if (error) return console.log('Hata:', error);
-    else if (!error) { 
-        var info = JSON.parse(body);
-request('https://www.doviz.com/api/v1/currencies/EUR/latest', function (error, response, body) {
-    if (error) return console.log('Hata:', error); 
-    else if (!error) { 
-        var euro = JSON.parse(body);
-      message.channel.send(`Dolar satış: ${info.selling} \nDolar alış: ${info.buying} \n\nEuro satış: ${euro.selling}TL \nEuro alış: ${euro.buying}TL`)    }
-})
-    }
-})
-    }
-
 client.on("message", async message => {
     const args = message.content.substring(prefix.length).split(" ");
     const command = args.shift().toLowerCase();
