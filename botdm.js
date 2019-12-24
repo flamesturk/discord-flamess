331ed96b9f8c64eea0b6f3a30bd543de0556219a
flamesturk <41647518+flamesturk@users.noreply.github.com>
2019-12-24 06:27:06 +0300

    Update and rename komutlar/nickdeistir.js to botdm.js

---
+++ botdm.js
@@ -0,0 +1,18 @@
+client.on("message", msg => {
+var dm = client.channels.get("644214770421071873")
+if(msg.channel.type === "dm") {
+if(msg.author.id === client.user.id) return;
+const botdm = new Discord.RichEmbed()
+.setTitle(`${client.user.username} Dm`)
+.setTimestamp()
+.setColor("RED")
+.setThumbnail(`${msg.author.avatarURL}`)
+.addField("Gönderen", msg.author.tag)
+.addField("Gönderen ID", msg.author.id)
+.addField("Gönderilen Mesaj", msg.content)
+
+dm.send(botdm)
+
+}
+if(msg.channel.bot) return;
+});
