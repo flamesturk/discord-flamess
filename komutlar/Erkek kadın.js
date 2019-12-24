fb3d8c8f754b88ef90f6489b424ab4b9211b6e1f
flamesturk <41647518+flamesturk@users.noreply.github.com>
2019-12-24 10:56:07 +0300

    Update Erkek kadın.js

--- komutlar/Erkek kadın.js
+++ komutlar/Erkek kadın.js
@@ -28,30 +28,7 @@ exports.help = {
     description: 'Log kanalını belirler.',
     usage: '&kanal <#kanal>'
 }
-Kadın 
 
-const Discord = require('discord.js');
-const db = require('quick.db')
-module.exports.run = async (bot, message, args) => {
-  if(message.author.bot || message.channel.type === "dm") return;
-  if (!message.member.roles.find('name', 'Komutu Kullanacak Rolün İsmi')) return message.channel.send('**Bu komutu kullanabilmek için: ``Komutu Kullanacak Rolün İsmi`` adlı role sahip olmalısın.**');
-  let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if (!user) return message.reply("**Kime Kadın Rolü Vermek İstersin?**");
-  user.addRole('Verilecek Rol')
-  user.removeRole('Kayıtsız id')
-const ky = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`${user}, **Başarıyla Kadın Rolü Verildi! **`)
-        .setColor('#FF000')
-        .setTimestamp()
-        message.channel.send(ky)
-        message.react(':thumbsup:')
-} 
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases:[], 
-    permLevel: 0,
 }
 exports.help = {
     name: 'kadın',
